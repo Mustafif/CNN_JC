683740bf9bc3b00e89206e3405430a5ec6e1fac2
Mustafif <mustafif0929@gmail.com>
2024-05-19 17:25:05 -0400

    created Data class for training, validation, and test data, built create and train model functions for Neural Network class

--- cnn_jc/cnn.py
+++ cnn_jc/cnn.py
@@ -8,12 +8,57 @@
 # reduceLRFactor: Factor to reduce learning rate by if no improvement is made
 # reduceLRPatience: Number of epochs to wait before reducing learning rate
 # reduceLRMin: Minimum learning rate
+from keras import Sequential
+from keras.api.initializers import glorot_uniform
+from keras.api.optimizers import Adam
+from keras.src.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.src.layers import Dense, ELU, Activation, Dropout
 
-from keras.initializers import glorot_uniform
 
-class NeuralNetwork(object): 
-    def __init__(self, data, method, loss, epochs, batch_size, activation='relu', layers=4, kernel_init=glorot_uniform, dropout=0.5, early_stop=125, lr_patience=40, reduce_lr=0.5, reduce_lr_min=0.000009,**kwargs):
+class Data:
+    def __init__(self, data):
         self.data = data
+        self.x_train = None
+        self.y_train = None
+        self.x_valid = None
+        self.y_valid = None
+        self.x_test = None
+
+    def split_data(self):
+        self.x_train, self.x_valid, self.x_test = self.data['x_train'], self.data['x_valid'], self.data['x_test']
+        self.y_train, self.y_valid = self.data['y_train'], self.data['y_valid']
+
+    def get_train_data(self):
+        return self.x_train, self.y_train
+
+    def get_valid_data(self):
+        return self.x_valid, self.y_valid
+
+    def get_test_data(self):
+        return self.x_test
+
+
+class NeuralNetwork(object):
+    def __init__(self, data, method, loss='mean_squared_error', epochs=0, batch_size=16, activation='relu', layers=4,
+                 kernel_init=glorot_uniform, dropout=0.5, early_stop=125, lr_patience=40, reduce_lr=0.5,
+                 reduce_lr_min=0.000009, **kwargs):
+        """
+        :param data (Data):  Data class containing the training, validation and testing data
+        :param method (keras.api.optimizers):  Optimizer to use for training
+        :param loss (str):  Loss function to use for training
+        :param epochs (int):  Number of epochs to train the model
+        :param batch_size (int):  Batch size for training
+        :param activation (str):  Activation function to use for hidden layers
+        :param layers (int):  Number of hidden layers in the model
+        :param kernel_init (keras.api.initializers):  Kernel initializer to use for hidden layers
+        :param dropout (float):  Dropout percentage to use for hidden layers
+        :param early_stop (int): Number of epochs to wait before stopping training if no improvement is made
+        :param lr_patience (int):  Number of epochs to wait before reducing learning rate
+        :param reduce_lr (float): Factor to reduce learning rate by if no improvement is made
+        :param reduce_lr_min (float): Minimum learning rate
+        :param kwargs (dict):  Additional keyword arguments
+        """
+        self.data = Data(data)
         self.method = method
         self.loss = loss
         self.epochs = epochs
@@ -27,4 +72,53 @@ class NeuralNetwork(object):
         self.reduce_lr = reduce_lr
         self.reduce_lr_min = reduce_lr_min
         self.kwargs = kwargs
-        
+
+    def create_model(self):
+        """
+        Creates the Calibration Neural Network model
+        """
+        x_train = self.data.get_train_data()[0]
+        x_valid = self.data.get_valid_data()[0]
+        x_test = self.data.get_test_data()
+        y_train = self.data.get_train_data()[1]
+        y_valid = self.data.get_valid_data()[1]
+
+        model = Sequential()
+        for i in range(self.layers):
+            if i == 0:
+                model.add(Dense(2 ** i, input_shape=(x_train.shape[1],), kernel_initializer=self.kernel_init))
+            else:
+                model.add(Dense(2 ** i, kernel_initializer=self.kernel_init))
+            if self.activation == 'elu':
+                model.add(ELU())
+            else:
+                model.add(Activation(self.activation))
+            model.add(Dropout(self.dropout))
+            model.add(Dense(y_train.shape[1], kernel_initializer=self.kernel_init))
+            model.compile(self.method, loss=self.loss)
+
+        return model
+
+    def train_model(self):
+        """
+        Trains the model
+        """
+        x_train, y_train = self.data.get_train_data()
+        x_valid, y_valid = self.data.get_valid_data()
+        x_test = self.data.get_test_data()
+        model = self.create_model()
+        if self.epochs > 0:
+            callbacks = []
+            if self.early_stop is not None:
+                early_stopping = EarlyStopping(monitor='val_loss', patience=self.early_stop)
+                callbacks.append(early_stopping)
+            if self.reduce_lr is not None:
+                reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=self.reduce_lr, patience=self.lr_patience,
+                                              min_lr=self.reduce_lr_min, verbose=1)
+                callbacks.append(reduce_lr)
+            history = model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size,
+                                validation_data=(x_valid, y_valid), callbacks=callbacks)
+        else:
+            history = None
+
+        return x_train, x_valid, x_test, model, history
