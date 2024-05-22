d8e82dc57938d9eed6491a7c00d5863ee0bfcd77
Mustafif <mustafif0929@gmail.com>
2024-05-21 20:33:31 -0400

    chore: Add research material and update NeuralNetwork class

---
+++ cnn_jc/__init__.py
@@ -0,0 +1,7 @@
+# The parameters we would want to calibrate are: 
+# theta = (lambda, omega, alpha, beta, gamma)
+
+# Will require data to be able to load options and returns data 
+# When we have a NeuralNetwork class, we can use the model returned 
+# from the `train_model` method, we can use `model.predict`. 
+
